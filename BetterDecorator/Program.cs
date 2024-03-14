b9b9aca4c8f4b4d95b40b337dbeb745024f3e331
Julia Costa <juliacosta232005@gmail.com>
2024-03-14 17:03:09 +0000

    Added Decor call and tested if args are working

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -7,13 +7,16 @@ namespace BetterDecorator
         static void Main(string[] args)
         {
             string str = args[0];
-            char chr = char.Parse(argrs[1]);
+            char chr = char.Parse(args[1]);
             int intg = int.Parse(args[2]);
+
+            Console.WriteLine(Decor(str,chr,intg));
             
         }
         private static string Decor(string s, char c, int i)
         {
-
+            string test = $"{s}, {c}, {i}";
+            return test;
         }
     }
 }
