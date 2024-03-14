0ac675f76bd67768f57067fd1cc699a747c0751d
Julia Costa <juliacosta232005@gmail.com>
2024-03-14 16:58:15 +0000

    Added conversion of args[] to desired formats

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -6,11 +6,14 @@ namespace BetterDecorator
     {
         static void Main(string[] args)
         {
+            string str = args[0];
+            char chr = char.Parse(argrs[1]);
+            int intg = int.Parse(args[2]);
             
         }
         private static string Decor(string s, char c, int i)
         {
-            
+
         }
     }
 }
