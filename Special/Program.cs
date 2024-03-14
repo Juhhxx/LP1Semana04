d4d2ae2e5ff3b99e20b53b5d7af340f81252b1c7
Julia Costa <juliacosta232005@gmail.com>
2024-03-14 17:54:11 +0000

    Added logic to the GetSpecial() method

--- Special/Program.cs
+++ Special/Program.cs
@@ -10,7 +10,18 @@ namespace Special
         }
         private static int GetSpecial(int n)
         {
-            
+            int result;
+
+            if (n == 0 || n == 1)
+            {
+                result = n;
+            }
+            else
+            {
+                result = GetSpecial(n - 1) + 2 * GetSpecial(n - 2);
+            }
+
+            return result;
         }
     }
 }
